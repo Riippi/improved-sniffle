c263f6744cacf975f3d0a390d094924ed0e9e6a8
Mikko Riippi <mikko.riippi@gofore.com>
2023-11-01 15:47:33 +0200

    Add functionality done with ChatGPT

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,91 @@
-import { useState } from 'react'
+import React, { useState } from 'react';
+import './App.css'
+
+interface BloodPressureMeasurement {
+  systolic: number;
+  diastolic: number;
+  pulse: number;
+}
+
+const BloodPressureTracker: React.FC = () => {
+  const [measurements, setMeasurements] = useState<BloodPressureMeasurement[]>([]);
+  const [currentMeasurement, setCurrentMeasurement] = useState<BloodPressureMeasurement>({
+    systolic: 0,
+    diastolic: 0,
+    pulse: 0,
+  });
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setCurrentMeasurement({
+      ...currentMeasurement,
+      [name]: Number(value),
+    });
+  };
+
+  const handleSaveMeasurement = () => {
+    setMeasurements([...measurements, currentMeasurement]);
+    setCurrentMeasurement({
+      systolic: 0,
+      diastolic: 0,
+      pulse: 0,
+    });
+  };
+
+  return (
+    <div>
+      <h2>Blood Pressure Tracker</h2>
+      <form>
+        <div>
+          <label>Systolic Pressure (mmHg):</label>
+          <input
+            type="number"
+            name="systolic"
+            value={currentMeasurement.systolic}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div>
+          <label>Diastolic Pressure (mmHg):</label>
+          <input
+            type="number"
+            name="diastolic"
+            value={currentMeasurement.diastolic}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div>
+          <label>Pulse (bpm):</label>
+          <input
+            type="number"
+            name="pulse"
+            value={currentMeasurement.pulse}
+            onChange={handleInputChange}
+          />
+        </div>
+        <button type="button" onClick={handleSaveMeasurement}>
+          Save Measurement
+        </button>
+      </form>
+      <div>
+        <h3>Measurements:</h3>
+        <ul>
+          {measurements.map((measurement, index) => (
+            <li key={index}>
+              Systolic: {measurement.systolic}, Diastolic: {measurement.diastolic}, Pulse: {measurement.pulse}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default BloodPressureTracker;
+
+
+
+/* import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -33,3 +120,4 @@ function App() {
 }
 
 export default App
+*/
